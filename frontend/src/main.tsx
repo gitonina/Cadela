3a663e245c4f407d7ef4d9734e84f220966c7bbe
ricardo-fernandez <ricfernandezreyes@gmail.com>
2025-09-23 21:42:30 -0300

    Set up React Router and routes
    Now the App only containt the routes to the pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from '@mui/material/styles'
+import { BrowserRouter as Router } from 'react-router-dom'
 import CssBaseline from '@mui/material/CssBaseline'
 import './index.css'
 import App from './App.tsx'
@@ -8,9 +9,11 @@ import theme from './themes.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Router>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <App />
       </ThemeProvider>
+    </Router>
   </StrictMode>,
 )
