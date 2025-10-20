a778af075c757c07bc3b791d52a5d97effaaf729
JVC2003 <jose.villalobos.c@ug.uchile.cl>
2025-10-20 14:03:18 -0300

    Add: inscription need login and simple button to add inscription

---
+++ frontend/src/components/SubscribeInscription.tsx
@@ -0,0 +1,4 @@
+import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
+import { createInscription } from "../services/inscriptions";
+import loginService from "../services/login";
