e2ce3cb6fa78540aff8376ff2c66fbc408d44f3d
ricardo-fernandez <ricfernandezreyes@gmail.com>
2025-10-19 21:51:36 -0300

    feat: add appbar, new routes and format file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,29 @@
-import { Container } from '@mui/material'
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import CalendarPage from './pages/CalendarPage'
-import ResultsPage from './pages/ResultsPage'
+import { Box, Container } from "@mui/material";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CalendarPage from "./pages/CalendarPage";
+import ResultsPage from "./pages/ResultsPage";
+import AdminPage from "./pages/AdminPage";
+import CadelaAppBar from "./components/AppBar";
+import ResultsListPage from "./pages/ResultsListPage";
 
 function App() {
   return (
-    <Container maxWidth="md">
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/calendar" element={<CalendarPage />} />
-        <Route path="/results/:raceId" element={<ResultsPage />} />
-      </Routes>
-    </Container>
-  )
+    <Box>
+      <CadelaAppBar />
+      <Container sx={{ mt: 4, flex: 1 }}>
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/calendar" element={<CalendarPage />} />
+          <Route path="/results" element={<ResultsListPage />} />
+          <Route path="/results/:raceId" element={<ResultsPage />} />
+          <Route path="/admin" element={<AdminPage />} />
+        </Routes>
+      </Container>
+    </Box>
+  );
 }
 
-export default App
+export default App;
