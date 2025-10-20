074a20b3278ac9b8b4ee30bf1be50f2a8d033c57
JVC2003 <jose.villalobos.c@ug.uchile.cl>
2025-10-20 12:46:09 -0300

    Add: category model and fix: inscriptions endpoints

---
+++ backend/src/models/category.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+interface Icategory {
+    id: string;
+    name: string;
+}
+
+const categorySchema = new mongoose.Schema<Icategory>({
+    name: {
+        type: String, 
+        required: true 
+    },
+});
+
+categorySchema.set("toJSON", {
+  transform: (
+    document,
+    returnedObject: { id?: string; _id?: mongoose.Types.ObjectId; __v?: number }
+  ) => {
+    returnedObject.id = returnedObject._id?.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+
+export const Circuit = mongoose.model("Category", categorySchema);
--- backend/src/models/cyclist.ts
+++ backend/src/models/cyclist.ts
@@ -1,11 +1,13 @@
 import mongoose,{Schema,Document} from 'mongoose'
 
 export interface ICyclist extends Document {
+  id: string,
   rut: number,
   name: string,
   club: string,
   n_dorsal: number,
   password: string,
+  inscriptions: mongoose.Types.ObjectId[];
 }
 
 const cyclistSchema = new Schema<ICyclist>({
@@ -20,11 +22,12 @@ const cyclistSchema = new Schema<ICyclist>({
   club: {type: String,required: true,minlength: 3},
   n_dorsal: {type: Number,required: true,minlength: 1,unique:true},
   password: {type: String,required: true,minlength: 6},
+  inscriptions: [{type: mongoose.Schema.Types.ObjectId, ref: 'Inscription' }]
 })
 
 
 cyclistSchema.set("toJSON", {
-  transform: (document: any, returnedObject: any) => {
+  transform: (document, returnedObject: any) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
@@ -32,6 +35,6 @@ cyclistSchema.set("toJSON", {
   },
 });
 
-const Cyclist = mongoose.model<ICyclist>('Usuario', cyclistSchema);
+const Cyclist = mongoose.model<ICyclist>('Cyclist', cyclistSchema);
 
 export default Cyclist;
