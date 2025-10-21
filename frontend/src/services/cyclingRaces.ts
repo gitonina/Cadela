d4a2fb045ef41702fb00daed7477c22ee512e25d
ricardo-fernandez <ricfernandezreyes@gmail.com>
2025-10-20 21:31:52 -0300

    feat: add more cyclingRaces services

--- frontend/src/services/cyclingRaces.ts
+++ frontend/src/services/cyclingRaces.ts
@@ -1,24 +1,43 @@
 import axios from "axios";
-import type { CyclingRace } from "../types/cyclingRace";
+import type { CyclingRace, CyclingRaceCreate } from "../types/cyclingRace";
 
-const baseUrl = "http://localhost:3001/cycling-races";
+const baseUrl = "http://localhost:3001/api/cycling-races";
 
 export const getAllRaces = async () => {
   const request = await axios.get<CyclingRace[]>(baseUrl);
   return request.data;
 };
 
+export const getUpcomingRaces = async () => {
+  const response = await axios.get<CyclingRace[]>(`${baseUrl}/upcoming`);
+  return response.data;
+}
+
+export const getPastRaces = async () => {
+  const response = await axios.get<CyclingRace[]>(`${baseUrl}/past`);
+  return response.data;
+}
+
+export const getNextRace = async () => {
+  const response = await axios.get<CyclingRace[]>(`${baseUrl}/next`);
+  return response.data[0] || null;
+}
+
 export const getRaceById = async (id: number) => {
   const response = await axios.get<CyclingRace>(`${baseUrl}/${id}`);
   return response.data;
 }
 
-export const create = async (newData: CyclingRace) => {
+export const create = async (newData: CyclingRaceCreate) => {
   const response = await axios.post<CyclingRace>(baseUrl, newData);
   return response.data;
 };
 
 export default {
   getAllRaces: getAllRaces,
+  getUpcomingRaces: getUpcomingRaces,
+  getPastRaces: getPastRaces,
+  getNextRace: getNextRace,
+  getRaceById: getRaceById,
   create: create,
 };
