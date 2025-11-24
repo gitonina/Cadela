02f52a62210829f70dcff89f44b69168a7f4e8b6
estebanrb22 <esteban.romero.b@ug.uchile.cl>
2025-11-23 21:05:26 -0300

    results frontend

--- frontend/src/components/CyclingRaceCard.tsx
+++ frontend/src/components/CyclingRaceCard.tsx
@@ -3,27 +3,46 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import { DirectionsBike } from "@mui/icons-material";
+import { Terrain } from "@mui/icons-material";
+import EventIcon from "@mui/icons-material/Event";
+
 
 import type { CyclingRace } from "../types/cyclingRace";
-import { formatDateNumber } from "../utils/dates";
+import { formatDateNumber, formatDateString } from "../utils/dates";
+import { Stack } from "@mui/material";
 
 const CyclingRaceCard = ({ race }: { race: CyclingRace }) => {
   return (
-    <Card sx={{ width: 600 }}>
+    <Card sx={{ width: 500 }}>
       <CardMedia sx={{ height: 180 }} image={race.circuitId.pathPhoto} />
-      <CardContent>
+      <CardContent sx={{padding: 2.3}}>
         <Box
           sx={{
             display: "flex",
-            justifyContent: "space-between",
             alignItems: "center",
+            flexDirection: "column",
+            placeItems: "flex-start",
             mx: 0.5,
           }}
         >
-          <Typography fontWeight="bold" sx={{ fontSize: 29 }}>
+
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="center"
+            sx={{ mb: 2 }}
+            gap={1}
+          >
+            <EventIcon sx={{ mr: 0.5, color: "#ad9090ff", fontSize: 30 }} />
+            <Typography variant="subtitle1" color="text.secondary" fontWeight="bold" sx={{  fontSize: 17}}>
+              {formatDateString(race.date)}
+            </Typography>
+          </Stack>
+
+          <Typography fontWeight="bold" sx={{ fontSize: 24 }}>
             {race.circuitId.name}
           </Typography>
-          <Typography variant="h5">{formatDateNumber(race.date)}</Typography>
         </Box>
         <Box
           sx={{
@@ -43,12 +62,18 @@ const CyclingRaceCard = ({ race }: { race: CyclingRace }) => {
               alignItems: "center",
             }}
           >
-            <Typography color="grey" variant="h6">
-              Distancia
-            </Typography>
-            <Typography color="grey" variant="h4" fontWeight="bold">
-              {race.circuitId.distance} km
-            </Typography>
+
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <DirectionsBike sx={{ fontSize: 60, mr: 1, color: 'primary.main' }} />
+              <Box sx={{ display: 'flex', flexDirection: "column", textAlign: "left" }}>
+                <Typography fontWeight="bold" sx={{ fontSize: 24, mb: 0, pb: 0 }}>
+                  {race.circuitId.distance} km
+                </Typography>
+                <Typography variant="caption" color="text.secondary" sx={{ fontSize: 16 }}>
+                  Distancia
+                </Typography>
+              </Box>
+            </Box>
           </Box>
           <Box
             sx={{
@@ -57,12 +82,17 @@ const CyclingRaceCard = ({ race }: { race: CyclingRace }) => {
               alignItems: "center",
             }}
           >
-            <Typography color="grey" variant="h6">
-              Desnivel positivo
-            </Typography>
-            <Typography color="grey" variant="h4" fontWeight="bold">
-              {race.circuitId.elevationGain} m
-            </Typography>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Terrain sx={{ fontSize: 70, mr: 1, color: 'error.main' }} />
+              <Box>
+                <Typography fontWeight="bold" sx={{ fontSize: 24, textAlign: "left", mb: 0, pb: 0 }}>
+                  {race.circuitId.elevationGain} m
+                </Typography>
+                <Typography variant="caption" color="text.secondary" sx={{ fontSize: 16 }}>
+                  Desnivel
+                </Typography>
+              </Box>
+            </Box>
           </Box>
         </Box>
       </CardContent>
